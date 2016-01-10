770ee214603b62690dd6ea8782409ffe60f8a50e
vagrant <vagrant@precise32.(none)>
2016-01-10 18:50:37 +0000

    Added NYT and Wiki apis

--- js/script.js
+++ js/script.js
@@ -6,11 +6,6 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
-    var $street = $('input[id=#street]');
-    var $city = $('input[for=#city]');
-    
-    console.log($street);
-    console.log($city);
 
     // clear out old data before new request
     $wikiElem.text("");
@@ -18,7 +13,57 @@ function loadData() {
 
     // load streetview
 
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='
+                            + address + '';
+    $body.append('<img class="bgimg" src=" ' + streetviewUrl +  '">');
+
     // YOUR CODE GOES HERE!
+
+    var apiKey = 'cbd70979b64299864c406d5728d1d082:1:73982500'
+    
+    var nytURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='
+                + cityStr + '&sort=newest&api-key=' + apiKey;
+
+    $.getJSON(nytURL, function(data) {
+        $nytHeaderElem.text('New York Times Articles about ' + cityStr);
+
+        articles = data.response.docs;
+
+        for(var i = 0; i < articles.length; ++i) {
+            var article = articles[i];
+
+            $nytElem.append('<li class="article">' +
+             '<a href="' + article.web_url + '">' + article.headline.main + '</a>' +  
+             '<p>' + article.snippet + '</p>' + 
+             '</li>') 
+        }
+    }).error(function() {
+        $nytHeaderElem.text("New York Times Articles could not be laoded");
+    })
+
+    var wikiURL = "http://en.wikipedia.org/w/api.php?action=opensearch&search=" + cityStr
+                + "&format=json&callback=wikicallback";
+
+    $.ajax({
+        url: wikiURL,
+        dataType: 'jsonp',
+        //jsonp: 'callback',
+        success: function (response) {
+            var articleList = response[1];
+
+            for(var i=0; i < articleList.length; ++i) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>')
+            }
+        }
+    })            
 
     return false;
 };
@@ -26,3 +71,6 @@ function loadData() {
 $('#form-container').submit(loadData);
 
 // loadData();
+
+
+//cbd70979b64299864c406d5728d1d082:1:73982500
